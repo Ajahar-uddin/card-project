6fd4af127314043213c5f3c2e2c4cedb0acc06af
Sk Ajahar <97507958+iskajju@users.noreply.github.com>
2023-06-09 23:16:38 +0530

    added

--- app/page.js
+++ app/page.js
@@ -2,11 +2,13 @@ import Container from "./Container";
 
 async function getCardData() {
   const res = await fetch(`/api/card`);
-  return res.json();
+  const data = await res.json()
+  return data;
 }
 
 export default async function Home() {
   const data = await getCardData();
+  console.log(data)
   return (
     <div>
       <Container data={data} />
