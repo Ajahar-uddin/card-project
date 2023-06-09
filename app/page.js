910ab750f621b22db77b9640342855205065538e
Sk Ajahar <97507958+iskajju@users.noreply.github.com>
2023-06-09 23:19:30 +0530

    change method

--- app/page.js
+++ app/page.js
@@ -1,14 +1,13 @@
 import Container from "./Container";
 
 async function getCardData() {
-  const res = await fetch(`/api/card`);
+  const res = await fetch(`https://randomuser.me/api/?inc=gender,name,nat,location,picture,email&results=%2020`);
   const data = await res.json()
   return data;
 }
 
 export default async function Home() {
   const data = await getCardData();
-  console.log(data)
   return (
     <div>
       <Container data={data} />
